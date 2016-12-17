861397a2568ae79096cc86aa43f5c2a915eb4f15
Damon <damon@mahuika.co.nz>
2016-12-17 18:14:14 +0700

    Remove Wallaby instrumentation from stack trace

--- index.js
+++ index.js
@@ -118,7 +118,8 @@ function config(wallaby) {
       wallaby.delayStart();
 
       process.on('unhandledRejection', function(reason, promise) {
-        console.error('Unhandled promise rejection', reason.stack || reason)
+        var exception = reason.stack ? reason.stack.replace(/\(\/.*?instrumented\//g, '(/') : reason
+        console.error('Unhandled promise rejection', exception)
       });
 
       var path = require('path');
